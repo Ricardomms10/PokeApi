6515e2f4cae0fb698afd5e81549db70e2188150b
Ricardomms10 <ricardomms10@gmail.com>
2023-05-19 16:06:29 -0300

    pokiapi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Home } from './components/pages/home';
+import FilePokemon from './components/filePokemon/FilePokemon';
+import Pokedex from './components/pokedex/pokedex';
+import { SearchPokemon, getPokemonData, getPokemons } from './components/searchPokemon/searchPokemon';
+import Button from './components/button/button';
+
+
 
 function App() {
+
+
+  const [loading, setLoading] = useState(false)
+  const [notFound, setNotFound] = useState(false)
+  const [pokemons, setPokemons] = useState([])
+  const [limit, setLimit] = useState(10)
+
+  const fetchPokemons = async () => {
+    try {
+      
+      setLoading(true)
+      setNotFound(false)
+      const data = await getPokemons(limit);
+      const promises = data.results.map(async (pokemon) => {
+        return await getPokemonData(pokemon.url)
+      });
+
+
+      const results = await Promise.all(promises);
+      setPokemons(results);
+      setLoading(false);
+    } catch (error) {
+      console.log('errou');
+    }
+    
+  }
+
+
+  useEffect(() => {
+    fetchPokemons()
+  },[limit])
+
+  
+  const onSearhHandler = async (pokemon) => {
+    if (!pokemon) {
+      return fetchPokemons();
+    }
+
+    setLoading(true)
+    setNotFound(false)
+    const result = await SearchPokemon(pokemon)
+    if (!result) {
+      setNotFound(true)
+    } else {
+      setPokemons([result])
+    }
+    setLoading(false)
+  }
+
+  const add = () =>{
+    setLimit(limit + 10)
+  }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Home />
+      <FilePokemon onSearch={onSearhHandler} />
+      {notFound ? (
+        <div>ih! esse não tem!</div>) :
+
+        (<Pokedex pokemons={pokemons} loading={loading} />)}
+      <Button Onclick={add} />
+     
+
     </div>
-  );
+
+  )
 }
 
-export default App;
+export default App
